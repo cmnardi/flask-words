3813ad89dd0916ef47abcaf14b09e782be256d07
Caio Nardi <cmnardi@gmail.com>
2023-02-07 22:41:02 -0300

    Testes - ordenar

--- tests/test_unit_words.py
+++ tests/test_unit_words.py
@@ -14,3 +14,15 @@ def test_vowel_count_coringa():
     input = "coringa"
     vowels = funcs.count_vowels(input)
     assert vowels == 3
+
+def test_sort_asc():
+    input = ["batman", "robin", "coringa"]
+    expected = ["batman", "coringa", "robin"]
+    result = funcs.order(input, "asc")
+    assert result == expected
+
+def test_sort_desc():
+    input = ["batman", "robin", "coringa"]
+    expected = ["robin", "coringa", "batman"]
+    result = funcs.order(input, "desc")
+    assert result == expected
