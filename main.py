a1091acaa4a964823a506c317e31bcf8e8d8bbe6
Caio Nardi <cmnardi@gmail.com>
2023-02-09 00:04:52 -0300

    sort - implementado endpoint

--- main.py
+++ main.py
@@ -20,11 +20,28 @@ class VowelCount(Resource):
             result[word] = funcs.count_vowels(word)
         return result
 
+class Sort(Resource):
+    def post(self):
+        request_data = request.get_json()
+        words = request_data["words"] if "words" in request_data else None
+        order = request_data["order"] if "order" in request_data else None
+        errors = []
+        if (words is None):
+            errors.append("Words field is required")
+        if (order is None):
+            errors.append("Order field is required")
+        elif (order not in ["asc", "desc"]):
+            errors.append("Order field must be asc or desc")
+        if errors:
+            return Response(json.dumps({"errors":errors}), status=400, mimetype='application/json')
+        return funcs.order(words, order)
+
 def create_app():
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(HealthCheck, '/')
     api.add_resource(VowelCount, '/vowel_count')
+    api.add_resource(Sort, '/sort')
     return app
 
 if __name__ == '__main__':
