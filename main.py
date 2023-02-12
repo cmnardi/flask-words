21cc590931e1c53e27e3533f25fc303f6ce42288
Caio Nardi <cmnardi@gmail.com>
2023-02-12 00:13:45 -0300

    Pylint (#5)
    * Pylint - aplicado no pipeline

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from flask import Flask, Response
-from flask_restful import Resource, Api
-from flask import request
+"""Api com funcões para manipular palavras."""
 import json
+
+from flask import Flask, Response, request
+from flask_restful import Resource, Api
+
 import funcs
-class HealthCheck(Resource):
+class HealthCheck(Resource): # pylint: disable=too-few-public-methods
+    """Testar se a api está ok."""
     def get(self):
+        """Testar se a api está ok."""
         return {"ok":200}
 
-
-class VowelCount(Resource):
+class VowelCount(Resource): # pylint: disable=too-few-public-methods
+    """Contar as vogais de cada palavra."""
     def post(self):
+        """Contar as vogais de cada palavra."""
         request_data = request.get_json()
         words = request_data["words"] if "words" in request_data else None
-        if (words is None):
+        if words is None:
             error = {"message": "Words field is required"}
             return Response(json.dumps(error), status=400, mimetype='application/json')
         result = {}
@@ -20,15 +25,17 @@ class VowelCount(Resource):
             result[word] = funcs.count_vowels(word)
         return result
 
-class Sort(Resource):
+class Sort(Resource): # pylint: disable=too-few-public-methods
+    """Ordenar as strings passadas."""
     def post(self):
+        """Ordenar as strings passadas."""
         request_data = request.get_json()
         words = request_data["words"] if "words" in request_data else None
         order = request_data["order"] if "order" in request_data else None
         errors = []
-        if (words is None):
+        if words is None:
             errors.append("Words field is required")
-        if (order is None):
+        if order is None:
             errors.append("Order field is required")
         elif (order not in ["asc", "desc"]):
             errors.append("Order field must be asc or desc")
